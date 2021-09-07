2364c060f26944acd3fc318e9c25d13d15ec4e04
Lars Karbo <mail@larskarbo.no>
2021-09-07 10:14:04 +0200

    ok

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -7,19 +7,19 @@ export function Layout({ children }) {
     <div className="bg-gray-900 min-h-screen relative flex flex-col items-center overflow-hidden">
       <div className="w-full bg-gray-800 border-b border-gray-500 mb-12">
         <div className="max-w-4xl px-8 py-4 mx-auto flex w-full justify-between items-center">
-          <div className="flex items-center">
+          <div >
             <Link href="/">
-              <a>
+              <a className="flex items-center">
                 <img src={"/chopper.svg"} className="w-16" />
+                <div className="text-2xl font-light mx-8">MediaChopper</div>
               </a>
             </Link>
-            <div className="text-2xl font-light mx-8">MediaChopper</div>
           </div>
           <div className="flex items-center gap-16">
             <Link href="/blog">
               <a className="text-sm hover:underline ">Blog</a>
             </Link>
-            
+
             <Link href="/#download">
               <a className="font-semibold text-white bg-blue-600 px-4 py-2 rounded shadow text-sm ">Download now</a>
             </Link>
