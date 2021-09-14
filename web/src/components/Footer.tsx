0901beab5111f86f6166fa862e7d46b2f55bf0f3
Lars Karbo <mail@larskarbo.no>
2021-09-14 14:19:17 +0200

    add alberto to footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,4 +1,3 @@
-
 export default function Footer() {
   return (
     <footer className="pt-28 w-full" aria-labelledby="footerHeading">
@@ -10,26 +9,28 @@ export default function Footer() {
           MediaChopper is a video editing platform that allows you to export individual clips from Davinci Resolve.
         </p>
         <div className="flex py-4">
-          <a href="https://larskarbo.no" target="_blank" rel="noreferrer">
-            <div
-              className=" flex items-center border border-gray-400 rounded p-2 px-4
-                  hover:border-gray-200 transition-colors duration-150 hover:shadow-sm
+          <div
+            className="font-light flex items-center border border-gray-400 rounded p-2 px-4
+                   transition-colors duration-150 hover:shadow-sm
                   "
-            >
-              <img
-                alt="Lars"
-                className="rounded-full mr-2 w-8"
-                src="https://s.gravatar.com/avatar/4579b299730ddc53e3d523ec1cd5482a?s=72"
-              />
-              <div className="font-light">
-                made by <strong className="font-bold">@larskarbo</strong>
-              </div>
-            </div>
-          </a>
+          >
+            made by
+            <img
+              alt="Lars"
+              className="rounded-full ml-2 w-8"
+              src="https://s.gravatar.com/avatar/4579b299730ddc53e3d523ec1cd5482a?s=72"
+            />
+            <a className="mx-2" href="https://larskarbo.no" target="_blank" rel="noreferrer">
+              <strong className="font-bold">@larskarbo</strong>
+            </a>
+            and
+            <a className="mx-2" href="https://twitter.com/alberto_villa" target="_blank" rel="noreferrer">
+              <strong className="font-bold">@alberto_villa</strong>
+            </a>
+          </div>
         </div>
         <div className="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
-          <div className="flex space-x-6 md:order-2">
-          </div>
+          <div className="flex space-x-6 md:order-2"></div>
           <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
             &copy; 2021 MediaChopper. All rights reserved.
           </p>
